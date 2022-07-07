92dc809ed72e83e56758f8d4bc6301cb988801ca
ju8ko <julia.x.kobzar@gmail.com>
2022-07-07 14:23:48 +0200

    Change the accessed arrays

--- solution.js
+++ solution.js
@@ -1,6 +1,6 @@
 const myArray = ["one", "two", "three", "four", "five"];
-const thirdElement = myArray[3];
+const thirdElement = myArray[2];
 console.log("My array:", myArray);
 console.log("Third Element from the 'my array':", thirdElement);
-myArray[4] = "fourth element";
+myArray[3] = "fourth element";
 console.log(myArray);
