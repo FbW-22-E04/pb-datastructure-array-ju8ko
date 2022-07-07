fdc51b0d7f5b555c7658798b469a6e98011f1915
ju8ko <julia.x.kobzar@gmail.com>
2022-07-07 11:28:42 +0200

    The exercise is done

---
+++ solution.js
@@ -0,0 +1,6 @@
+const myArray = ["one", "two", "three", "four", "five"];
+const thirdElement = myArray[3];
+console.log("My array:", myArray);
+console.log("Third Element from the 'my array':", thirdElement);
+myArray[4] = "fourth element";
+console.log(myArray);
